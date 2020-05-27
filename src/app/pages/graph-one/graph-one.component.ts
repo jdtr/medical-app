c049bfa743cfbea81bedd0cbf673cd5298d8b1c4
Diego Torres <dtorres@chefcompany.co>
2020-05-26 22:12:06 -0500

    Start project

---
+++ src/app/pages/graph-one/graph-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-one',
+  templateUrl: './graph-one.component.html',
+  styles: []
+})
+export class GraphOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
